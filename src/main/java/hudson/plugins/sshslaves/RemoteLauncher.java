e6ee21a64f1c91184b2b3d888e5dc8d30422a690
8nevil8 <nevil.nick@gmail.com>
2011-05-04 12:07:49 +0300

    Add pmd plugin, resolve pmd errors. Update developers section

--- src/main/java/hudson/plugins/sshslaves/RemoteLauncher.java
+++ src/main/java/hudson/plugins/sshslaves/RemoteLauncher.java
@@ -7,13 +7,10 @@ import hudson.FilePath;
 import hudson.Launcher;
 import hudson.Proc;
 import hudson.Util;
-import hudson.Functions;
-import static hudson.Functions.defaulted;
-import hudson.util.StreamCopyThread;
 import hudson.model.Computer;
 import hudson.model.TaskListener;
 import hudson.remoting.Channel;
-
+import hudson.util.StreamCopyThread;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -21,10 +18,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import static hudson.Functions.defaulted;
+
 /**
  * Pseudo-{@link Launcher} implementation over SSH.
- *
- * <p>
+ * <p/>
+ * <p/>
  * Currently this code only has enough to make JDK auto-installation work.
  *
  * @author Kohsuke Kawaguchi
@@ -33,7 +32,7 @@ final class RemoteLauncher extends Launcher {
     private final Connection connection;
 
     public RemoteLauncher(TaskListener listener, Connection connection) {
-        super(listener,null);
+        super(listener, null);
         this.connection = connection;
     }
 
@@ -45,17 +44,18 @@ final class RemoteLauncher extends Launcher {
         String name = ps.cmds().toString();
 
         final Session session = connection.openSession();
-        session.execCommand(makeCommandLine(ps.cmds(),ps.pwd()));
-        final Thread t1 = new StreamCopyThread("stdout copier: "+name,session.getStdout(), ps.stdout(),false);
+        session.execCommand(makeCommandLine(ps.cmds(), ps.pwd()));
+        final Thread t1 = new StreamCopyThread("stdout copier: " + name, session.getStdout(), ps.stdout(), false);
         t1.start();
-        final Thread t2 = new StreamCopyThread("stderr copier: "+name,session.getStderr(), defaulted(ps.stderr(),ps.stdout()),false);
+        final Thread t2 = new StreamCopyThread("stderr copier: " + name, session.getStderr(),
+            defaulted(ps.stderr(), ps.stdout()), false);
         t2.start();
-        final Thread t3 = new StreamCopyThread("stdin copier: "+name,ps.stdin(), session.getStdin(),true);
+        final Thread t3 = new StreamCopyThread("stdin copier: " + name, ps.stdin(), session.getStdin(), true);
         t3.start();
 
         return new Proc() {
             public boolean isAlive() throws IOException, InterruptedException {
-                return session.getExitStatus()==null;
+                return session.getExitStatus() == null;
             }
 
             public void kill() throws IOException, InterruptedException {
@@ -70,9 +70,11 @@ final class RemoteLauncher extends Launcher {
                     t1.join();
                     t2.join();
                     t3.join();
-                    session.waitForCondition(ChannelCondition.EXIT_STATUS,0);
+                    session.waitForCondition(ChannelCondition.EXIT_STATUS, 0);
                     Integer r = session.getExitStatus();
-                    if(r!=null) return r;
+                    if (r != null) {
+                        return r;
+                    }
                     return -1;
                 } finally {
                     session.close();
@@ -82,19 +84,20 @@ final class RemoteLauncher extends Launcher {
     }
 
 
-    public Channel launchChannel(String[] cmd, OutputStream out, FilePath _workDir, Map<String, String> envVars) throws IOException, InterruptedException {
+    public Channel launchChannel(String[] cmd, OutputStream out, FilePath _workDir, Map<String, String> envVars)
+        throws IOException, InterruptedException {
         printCommandLine(cmd, _workDir);
 
         final Session session = connection.openSession();
         session.execCommand(makeCommandLine(Arrays.asList(cmd), _workDir));
 
-        return new Channel("channel over ssh on "+connection.getHostname()+":"+connection.getPort(),
+        return new Channel("channel over ssh on " + connection.getHostname() + ":" + connection.getPort(),
             Computer.threadPoolForRemoting, session.getStdout(), new BufferedOutputStream(session.getStdin()));
     }
 
     private String makeCommandLine(List<String> cmd, FilePath _workDir) {
-        final String workDir = _workDir==null ? null : _workDir.getRemote();
-        return "cd '" + workDir + "' && " + Util.join(cmd," "); // TODO: quote handling
+        final String workDir = _workDir == null ? null : _workDir.getRemote();
+        return "cd '" + workDir + "' && " + Util.join(cmd, " "); // TODO: quote handling
     }
 
     public void kill(Map<String, String> modelEnvVars) throws IOException, InterruptedException {
